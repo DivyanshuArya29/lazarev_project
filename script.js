fb3f70f21fd3ac4f1b9acf9cfd67807a469fac8d
DivyanshuArya <aryadivyanshu29@gmail.com>
2025-07-24 18:38:00 +0530

    page2,3 done

--- script.js
+++ script.js
@@ -38,4 +38,90 @@ function navAnimation() {
     })
 }
 
+function page2Animation() {
+    var relem = document.querySelector("#right-elem")
+    var relemImg = document.querySelector("#right-elem img")
+
+    relem.addEventListener("mouseenter", function () {
+        relemImg.style.opacity = 1
+    })
+
+    var rightElems = document.querySelectorAll(".right-elem")
+
+    rightElems.forEach(function (elem) {
+        elem.addEventListener("mouseenter", function () {
+            elem.childNodes[3].style.opacity = 1
+            console.log("Added")
+        })
+        elem.addEventListener("mouseleave", function () {
+            elem.childNodes[3].style.opacity = 0
+            console.log("Removed")
+        })
+        elem.addEventListener("mouseleave", function () {
+            gsap.to(elem.childNodes[3],)
+        })
+    })
+
+    var rightElems = document.querySelectorAll(".right-elem")
+
+    rightElems.forEach(function (elem) {
+        // console.log(elem)
+        elem.addEventListener("mouseenter", function () {
+
+            elem.childNodes[3].style.opacity = 1
+        })
+        elem.addEventListener("mouseleave", function () {
+            console.log("Removed")
+        })
+    })
+}
+
+function page3VideoAnimation() {
+    var page3Center = document.querySelector(".page3-center")
+    var video = document.querySelector("#page3 video")
+
+    page3Center.addEventListener("click", function () {
+        video.play()
+        gsap.to(video, {
+            transform: "scaleX(1) scaleY(1)",
+            opacity: 1,
+            borderRadius: 0
+        })
+    })
+    video.addEventListener("click", function () {
+        video.pause()
+        gsap.to(video, {
+            transform: "scaleX(0.7) scaleY(0)",
+            opacity: 0,
+            borderRadius: "30px"
+        })
+    })
+
+
+    var sections = document.querySelectorAll(".sec-right")
+
+    sections.forEach(function (elem) {
+        elem.addEventListener("mouseenter", function () {
+            elem.childNodes[3].style.opacity = 1
+            elem.childNodes[3].play()
+        })
+        elem.addEventListener("mouseleave", function () {
+            elem.childNodes[3].style.opacity = 0
+            elem.childNodes[3].load()
+        })
+    })
+
+}
+
 // navAnimation()
+
+// page2Animation()
+
+page3VideoAnimation()
+
+
+
+
+
+
+
